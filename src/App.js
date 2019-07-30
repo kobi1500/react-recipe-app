27510d4af8bf12492fb1f476e95579f1425509ad
kobi dahan <kobi6255@gmail.com>
2019-07-30 10:42:32 +0300

    create navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipes";
+import Default from "./pages/Default";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
 function App() {
-  return <h1>hello from our recipe app</h1>;
+  return (
+    <Router>
+      <main>
+        {/* Navbar */}
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/recipes" exact component={Recipes} />
+          <Route path="/recipes/:id" component={SingleRecipe} />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
