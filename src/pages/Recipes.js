e31544f3b33e0fa4cd8e3bcb4acfd6944b410e86
kobi dahan <kobi6255@gmail.com>
2019-07-30 14:37:25 +0300

    finish recipes

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -5,11 +5,44 @@ import { recipeData } from "../data/tempList";
 export default class Recipes extends Component {
   constructor(props) {
     super(props);
+    this.getRecipes = this.getRecipes.bind(this);
   }
   state = {
     recipes: recipeData,
-    search: ""
+    search: "",
+    url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    base_url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    query: "&q=",
+    error: ""
   };
+
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "sorry but your search did not return any recipes, please try again or press search icon for most popular recipes"
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes,
+          error: ""
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.getRecipes();
+  }
 
   handleChange = e => {
     this.setState({
@@ -19,6 +52,14 @@ export default class Recipes extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`,
+        search: ""
+      },
+      () => this.getRecipes()
+    );
   };
 
   render() {
@@ -29,7 +70,19 @@ export default class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
-        <RecipeList recipes={this.state.recipes} />
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipes={this.state.recipes} />
+        )}
       </>
     );
   }
